ae2e588dde311dfbea98e733232b5b23a36eef4c
unknown <msadi7573@gmail.com>
2019-11-06 15:19:50 +0600

    google scraping added

--- extract.py
+++ extract.py
@@ -1,9 +1,26 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-
+import time
 def casemaker(msg):
     return msg.lower()
+
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def getwikifilter(wiki_uri):
+    resp= requests.get(wiki_url)
+    soup = BeautifulSoup(resp.content)
+    blocks= soup.findAll('p')
+    result=""
+    for block in blocks:
+        if(len(cleanhtml(str(block)))>100):
+            result= (cleanhtml(str(block)))
+            break
+    return result
 
 def wikisp(msg):
     typo=0
@@ -30,39 +47,11 @@ def wikisp(msg):
     print "link of capitalized"
     print capitalized
     wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
-    resp= requests.get(wiki_url)
-    soup = BeautifulSoup(resp.content)
-    blocks= soup.findAll('p')
-    result=""
-    for block in blocks:
-    	if ("Narendra" in str(block) and "Modi" in str(block)):
-    		print (str(block))
-    #print str(blocks)
-    for block in blocks:
-    	#lower=casemaker(block)
-    	blk=str(block)
-    	print block
-    	print blk
-    	flag=1
-    	for word in words:
-    		w=word.title()
-    		if w not in blk:
-    			print "\n\n"
-    			print w
-    			print "|||||Here||||||"
-    			print blk
-    			print "\n\n"
-    			flag=0
-    	if (flag==1):
-    		result=blk
-    		break
-    print "Result is:"+str(result)
+    result=getwikifilter(wiki_uri)
+    return result
 
-
-
-
-def wiki(msg):
-    typo=0
+def autocomplete(msg):
+    retvar={}
     if ("what is the" in msg ):
         typo=1
         msg=msg.replace("what is ","")
@@ -78,35 +67,109 @@ def wiki(msg):
 
     msg=msg.replace("?","")
     msg= msg.strip()
-    msg= " ".join(msg.split())
-    words = msg.split(' ')
-    whitelist = {'and', 'the', 'is', 'of'}
-    capitalized = ' '.join([word.title() if word not in whitelist else word for word in words])
-    capitalized=capitalized.replace(" ","_")
-    print "link of capitalized"
-    print capitalized
-    wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
-    resp= requests.get(wiki_url)
-    content= str(resp.content)
-    print content
-    blocks = re.findall(r'<p>(.*?)?</p>', content)
-    result=""
-    print blocks
-    for block in blocks:
-    	block=casemaker(str(block))
-    	f=1
-    	for w in words:
-    		print "w is:"+str(w)
-    		print "block is:"+str(block)
-    		if w!=block:
-    			f=0
-    	if f==1:
-    		result=block
-    print "result is: "
-    print result
-    return result
+    msg=msg.replace(" ","+")
+
+    url="https://www.google.com/search?q="+str(msg)
+    resp= requests.get(url)
+    cont= str(resp.content)
+    wikiuri=""
+    try:
+        an= re.search(r"{var q='(.*?)?';", str(cont)).group(1)
+        retvar["corrected"]=an
+    except:
+        retvar["corrected"]=msg
+    try:
+        wiki= re.findall(r'<div class="kCrYT"><a href="(.*?)?"><div class="BNeawe vvjwJb AP7Wnd', cont)
+        for wik in wiki:
+            #print wik
+            #print "\n-------------\n"
+            if("en.wikipedia" in wik):
+                wikiuri= wik
+                wikiuri=wikiuri.replace("/url?q=","")
+                #break
+        retvar["wikiuri"]=wikiuri.split("&")[0]
+    except:
+         retvar["wikiuri"]=""
+    return retvar
+    #print an
+   # print  wiki
+
+    #print str(blocks)
+    # for block in blocks:
+    # 	#lower=casemaker(block)
+    # 	blk=str(block)
+    # 	print block
+    # 	print blk
+    # 	flag=1
+    # 	for word in words:
+    # 		w=word.title()
+    # 		if w not in blk:
+    # 			print "\n\n"
+    # 			print w
+    # 			print "|||||Here||||||"
+    # 			print blk
+    # 			print "\n\n"
+    # 			flag=0
+    # 	if (flag==1):
+    # 		result=blk
+    # 		break
+    #print "Result is:"+str(result)
 
 
 
-#msg= raw_input("msg:")
-print wikisp(casemaker("narendra modi"))
+
+# def wiki(msg):
+#     typo=0
+#     if ("what is the" in msg ):
+#         typo=1
+#         msg=msg.replace("what is ","")
+#     elif("what's the" in msg):
+#         typo=2
+#         msg= msg.replace("what's ","")
+#     elif("who's" in msg):
+#         typo=3
+#         msg= msg.replace("who's ","")
+#     elif("who is" in msg):
+#         typo=4
+#         msg= msg.replace("who is ","")  
+
+#     msg=msg.replace("?","")
+#     msg= msg.strip()
+#     msg= " ".join(msg.split())
+#     words = msg.split(' ')
+#     whitelist = {'and', 'the', 'is', 'of'}
+#     capitalized = ' '.join([word.title() if word not in whitelist else word for word in words])
+#     capitalized=capitalized.replace(" ","_")
+#     print "link of capitalized"
+#     print capitalized
+#     wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
+#     resp= requests.get(wiki_url)
+#     content= str(resp.content)
+#     print content
+#     blocks = re.findall(r'<p>(.*?)?</p>', content)
+#     result=""
+#     print blocks
+#     for block in blocks:
+#     	block=casemaker(str(block))
+#     	f=1
+#     	for w in words:
+#     		print "w is:"+str(w)
+#     		print "block is:"+str(block)
+#     		if w!=block:
+#     			f=0
+#     	if f==1:
+#     		result=block
+#     print "result is: "
+#     print result
+#     return result
+
+
+
+# msg= raw_input("msg:")
+# print wikisp(casemaker(msg))
+ok= autocomplete("mithila")
+
+
+
+print ok["corrected"]
+print ok["wikiuri"]
--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@
 from nltk.chat.util import Chat, reflections
 import json
 import os, re
+from bs4 import BeautifulSoup
 import requests
 from fuzzywuzzy import fuzz
 # Third party libraries
@@ -104,18 +105,12 @@ pairs = [
         ["Messy","Ronaldo","Roony"]
 ],
     [
-        r"who (.*) (moviestar|actor)?",
-        ["Brad Pitt"]
-],
-    [
         r"quit",
         ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
 ],
 ]
 
-with open('F:\Personal-site\simba\data\dictionary.txt') as f:
-	lines = f.readlines()
-f.close()
+
 chat = Chat(pairs, reflections)
 
 
@@ -137,6 +132,102 @@ chat = Chat(pairs, reflections)
 
 def casemaker(msg):
     return msg.lower()
+
+def clearref(text):
+  cleanr = re.sub(r'\[.*\]', '', text)
+  print cleanr
+  return cleanr
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+  cleantext = re.sub(cleanr, '', raw_html)
+  #return clearref(cleantext)
+  for i in range(1,25):
+    cleantext=cleantext.replace("["+str(i)+"]","")
+  return cleantext
+
+
+
+def getwikifilter(wiki_uri):
+    resp= requests.get(wiki_uri)
+    soup = BeautifulSoup(resp.content)
+    blocks= soup.findAll('p')
+    result=""
+    for block in blocks:
+        if(len(cleanhtml(str(block)))>100):
+            result= (cleanhtml(str(block)))
+            break
+    return result
+
+def wikisp(msg):
+    typo=0
+    if ("what is the" in msg ):
+        typo=1
+        msg=msg.replace("what is ","")
+    elif("what's the" in msg):
+        typo=2
+        msg= msg.replace("what's ","")
+    elif("who's" in msg):
+        typo=3
+        msg= msg.replace("who's ","")
+    elif("who is" in msg):
+        typo=4
+        msg= msg.replace("who is ","")  
+
+    msg=msg.replace("?","")
+    msg= msg.strip()
+    msg= " ".join(msg.split())
+    words = msg.split(' ')
+    whitelist = {'and', 'the', 'is', 'of'}
+    capitalized = ' '.join([word.title() if word not in whitelist else word for word in words])
+    capitalized=capitalized.replace(" ","_")
+    print "link of capitalized"
+    print capitalized
+    wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
+    result=getwikifilter(wiki_uri)
+    return result
+
+def autocomplete(msg):
+    retvar={}
+    if ("what is the" in msg ):
+        typo=1
+        msg=msg.replace("what is ","")
+    elif("what's the" in msg):
+        typo=2
+        msg= msg.replace("what's ","")
+    elif("who's" in msg):
+        typo=3
+        msg= msg.replace("who's ","")
+    elif("who is" in msg):
+        typo=4
+        msg= msg.replace("who is ","")  
+
+    msg=msg.replace("?","")
+    msg= msg.strip()
+    msg=msg.replace(" ","+")
+
+    url="https://www.google.com/search?q="+str(msg)
+    resp= requests.get(url)
+    cont= str(resp.content)
+    wikiuri=""
+    try:
+        an= re.search(r"{var q='(.*?)?';", str(cont)).group(1)
+        retvar["corrected"]=an
+    except:
+        retvar["corrected"]=msg
+    try:
+        wiki= re.findall(r'<div class="kCrYT"><a href="(.*?)?"><div class="BNeawe vvjwJb AP7Wnd', cont)
+        for wik in wiki:
+            #print wik
+            #print "\n-------------\n"
+            if("en.wikipedia" in wik):
+                wikiuri= wik
+                wikiuri=wikiuri.replace("/url?q=","")
+                #break
+        retvar["wikiuri"]=wikiuri.split("&")[0]
+    except:
+         retvar["wikiuri"]=""
+    return retvar
 
 
 def ans(msg):
@@ -163,47 +254,74 @@ def ans(msg):
 
 
 def reply(msg):
-	maxm=0
-	repl=""
-	global lines
-	for line in lines:
-		parts=line.split(">>")
-		rat= fuzz.ratio( parts[0], msg)
-		if rat>maxm:
-			maxm=rat
-			repl=parts[1]
+    maxm=0
+    repl=""
+    with open('data/dictionary.txt') as f:
+        lines=f.readlines()
+    f.close()
+    print "Lines"
+    print lines
+    for line in lines:
+        print "1."+str(line)
+        parts= line.split(">>")
+        parts[0]=parts[0].strip()
+        print parts
+        rat= fuzz.ratio(str(parts[0]), str(msg))
+        print "for msg:"+str(msg)
+        if rat>maxm:
+            maxm=rat
+            repl= parts[1]
+        print rat
+    return repl
+    # for line in lines:
+    #     print "Line is: "+str(line)
+    #     parts= line.split(">>")
+    #     #parts=line.ratio(parts[0], msg)
+    #     rat= fuzz.ratio(parts[0], msg)
+    #     print str(parts[0])+":"+str(rat)
+    #     if rat>maxm:
+    #         maxm=ratio
+    #         repl= parts[1]
+    # return repl
+		# parts=line.split(">>")
+  #       rat= fuzz.ratio(parts[0], msg)
+   #      rat= fuzz.ratio( parts[0], msg)
+   #      print str(parts[0])+": "+str(rat)
+   #      if rat>maxm:
+			# maxm=rat
+			# repl=parts[1]
 
 
-	return repl
+	# return repl
 
 
-def wiki(msg):
-    typo=0
-    if ("what is the" in msg ):
-        typo=1
-        msg=msg.replace("what is ","")
-    elif("what's the" in msg):
-        typo=2
-        msg= msg.replace("what's ","")
-    elif("who's" in msg):
-        typo=3
-        msg= msg.replace("who's ","")
-    elif("who is" in msg):
-        typo=4
-        msg= msg.replace("who is ","")  
+# def wiki(msg):
+#     typo=0
+#     if ("what is the" in msg ):
+#         typo=1
+#         msg=msg.replace("what is ","")
+#     elif("what's the" in msg):
+#         typo=2
+#         msg= msg.replace("what's ","")
+#     elif("who's" in msg):
+#         typo=3
+#         msg= msg.replace("who's ","")
+#     elif("who is" in msg):
+#         typo=4
+#         msg= msg.replace("who is ","")  
 
-    msg=msg.replace("?","")
-    msg= msg.strip()
-    msg= " ".join(msg.split())
-    words = msg.split(' ')
-    whitelist = {'and', 'the', 'is', 'of'}
-    capitalized = ' '.join([word.title() if word not in whitelist else word for word in words])
-    capitalized=capitalized.replace(" ","_")
-    print "link of capitalized"
-    print capitalized
-    wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
-    resp= requests.get(url)
-    content= resp.content
+#     msg=msg.replace("?","")
+#     msg= msg.strip()
+#     msg= " ".join(msg.split())
+#     words = msg.split(' ')
+#     whitelist = {'and', 'the', 'is', 'of'}
+#     capitalized = ' '.join([word.title() if word not in whitelist else word for word in words])
+#     capitalized=capitalized.replace(" ","_")
+#     print "link of capitalized"
+#     print capitalized
+#     wiki_url= "https://en.wikipedia.org/wiki/"+capitalized
+#     resp= requests.get(url)
+#     content= resp.content
 
 
 
@@ -218,26 +336,48 @@ def index():
 	return render_template('simba.html')
 
 @app.route('/post',methods = ['POST', 'GET'])
-def msg():
-	try:
-		message="null"
-		if request.method == 'POST':
-			 messagemix = str(request.form['msg'])
-			 message=casemaker(messagemix)
-			 msg=simba(message)
-			 if ("what is" in message or "what's" in message) and (("your" not in message) and ("is the" not in message)):
-			 	print "here going to ans"
-			 	msg= ans(message)
-             # elif("who is" in message or "who's" in message or "what is the" in message or "what's the" in message) and (("your" not in message)):
-             #    print "wiki is going on"
-             #    msg=wiki(message)
-
-
-			 if msg==None:
-			 	msg= reply(message)
-			 return msg
-	except:
-		return "Sorry, that's beyond my knowledge"
+def  msg():
+    try:
+        message="null"
+        if request.method =="POST":
+            messagemix=str(request.form['msg'])
+            message= casemaker(messagemix)
+            msg= simba(message)
+            if ("what is" in message or "what's" in message) and (("your" not in message) and ("is the" not in message)):
+                print "Here going to dict"
+                msg=ans(message)
+            elif("who is" in message or "who's" in message or "what is the" in message or "what's the" in message) and (("your" not in message)):
+                print "Wiki is going on"
+                ok= autocomplete(message)
+                if(ok["wikiuri"]!=""):
+                    msg=getwikifilter(ok["wikiuri"])
+                else:
+                    msg=wikisp(casemaker(message))
+            if msg==None or msg=="":
+                print 'going to reply'
+                msg=reply(message)
+            print msg
+            return msg
+    except:
+        return "Sorry, that's beyond my knowledge"
+# def msg():
+# 	try:
+# 		message="null"
+# 		if request.method == 'POST':
+# 			 messagemix = str(request.form['msg'])
+# 			 message=casemaker(messagemix)
+# 			 msg=simba(message)
+# 			 if ("what is" in message or "what's" in message) and (("your" not in message) and ("is the" not in message)):
+# 			 	print "here going to ans"
+# 			 	msg= ans(message)
+#     #          elif("who is" in message or "who's" in message or "what is the" in message or "what's the" in message) and (("your" not in message)):
+#     #             print "wiki is going on"
+#     #             msg=wiki(message)
+# 			 # if msg==None:
+# 			 # 	msg= reply(message)
+# 			 return msg
+# 	except:
+# 		return "Sorry, that's beyond my knowledge"
 
 if __name__ == "__main__":
     app.run(debug = True)
